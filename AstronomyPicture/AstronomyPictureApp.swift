1880d32a4a5729282b1748c41d8466a14449e947
AndreyKaldyaev <59248096+AndreyK-16@users.noreply.github.com>
2023-01-30 22:19:04 +0300

    init commit

---
+++ AstronomyPicture/AstronomyPictureApp.swift
@@ -0,0 +1,17 @@
+//
+//  AstronomyPictureApp.swift
+//  AstronomyPicture
+//
+//  Created by Andrey Kaldyaev on 30.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct AstronomyPictureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
